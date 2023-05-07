3e7370c89544cd4012f3df6019d504ff60c002c6
Muhammad Furqan Soedono <muhammad.furqan@ai.astra.co.id>
2023-05-07 14:28:42 +0700

    add typeorm service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,23 @@ import { AppService } from './app.service';
 import { LeadsModule } from './modules/leads/leads.module';
 import { UsersModule } from './modules/users/users.module';
 import { UsersController } from './modules/users/users.controller';
+import { SharedModule } from './shared/shared.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiConfigService } from './shared/services/api-config.services';
 
+const ApiModules = [LeadsModule, UsersModule, SharedModule];
 @Module({
-  imports: [ConfigModule.forRoot(), LeadsModule, UsersModule],
+  imports: [
+    ...ApiModules,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [SharedModule],
+      useFactory: (configService: ApiConfigService) => {
+        return configService.mssqlConfig;
+      },
+      inject: [ApiConfigService],
+    }),
+  ],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
