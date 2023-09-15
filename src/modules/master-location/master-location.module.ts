2dbed07b67e10827ab8960eb2c11c6c617a64332
Aryasa Miftah Mubaraq Siagian <aryasa.miftah@ai.astra.co.id>
2023-09-15 13:51:33 +0700

    feat: Add Location (CRUD)

--- src/modules/master-location/master-location.module.ts
+++ src/modules/master-location/master-location.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { LocationService } from './master-location.service';
 import { LocationController } from './master-location.controller';
+import { Location } from './master-location.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Location])],
   providers: [LocationService],
   controllers: [LocationController],
 })
-export class MasterLocationModule {}
+export class LocationModule {}
