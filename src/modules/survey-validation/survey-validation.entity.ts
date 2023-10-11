b2fb72ceeaedfd27a3d7b550dd8cf0facc822ba9
Aryasa Miftah Mubaraq Siagian <aryasa.miftah@ai.astra.co.id>
2023-10-11 16:12:12 +0700

    fix: Add Cleansing Survey

--- src/modules/survey-validation/survey-validation.entity.ts
+++ src/modules/survey-validation/survey-validation.entity.ts
@@ -1,3 +1,25 @@
-// import { Entity } from "typeorm";
+import { Column, Entity } from 'typeorm';
 
-// @Entity('')
+@Entity('tbl_surveyvalidation')
+export class SurveyValidation {
+  @Column({ type: 'varchar', name: 'validation', nullable: false })
+  validation: string;
+
+  @Column({ type: 'varchar', name: 'company', nullable: false })
+  company: string;
+
+  @Column({ type: 'varchar', name: 'surveyid', nullable: false })
+  surveyid: string;
+
+  @Column({ type: 'varchar', name: 'titlesurvey', nullable: false })
+  titlesurvey: string;
+
+  @Column({ type: 'varchar', name: 'dateversion', nullable: false })
+  dateversion: Date;
+
+  @Column({ type: 'varchar', name: 'username', nullable: false })
+  username: string;
+
+  @Column({ type: 'varchar', name: 'validateddate', nullable: false })
+  validateddate: Date;
+}
