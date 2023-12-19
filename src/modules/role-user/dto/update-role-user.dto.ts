5254d0117d497cbc9c280e7f4b5c4ed79d4487f9
furqan <muhammad.furqan@ai.astra.co.id>
2023-12-19 14:36:22 +0700

    fix: add api property add roleuser

--- src/modules/role-user/dto/update-role-user.dto.ts
+++ src/modules/role-user/dto/update-role-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { AddRoleUserDto } from './add-role-user.dto';
 import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
@@ -6,6 +6,7 @@ import { Type } from 'class-transformer';
 export class UpdateRoleUserDto extends PartialType(AddRoleUserDto) {}
 
 export class SyncRoleUserDTO {
+  @ApiProperty({ type: [AddRoleUserDto] })
   @IsArray({ message: 'Input must be an array' })
   @ValidateNested({ each: true })
   @ArrayMinSize(1, { message: 'User must have at least 1 role' })
